7779451d58bd20f1e2b8ae0bd15a82376adc1707
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-05-06 18:08:42 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   random_num = rand(6) + 1
   puts "Guess a number from 1 - 6!"
-  input = gets.to_i 
+  # input = gets.to_i 
   if input == random_num
     puts "You guessed the correct number!"
     elsif input == "exit"
