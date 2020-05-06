bcc20202dd8a8ce5b95d867db57748a5c585857f
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-05-06 18:00:51 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,10 +5,10 @@ def run_guessing_game
   input = gets.to_i 
   if input == random_num
     puts "You guessed the correct number!"
-    elsif input == "exit"
-    puts "Goodbye!"
   else 
     puts "Sorry! The computer guessed #{random_num}."
   end 
-  
+  input = gets 
+  if input == "exit"
+    puts "Goodbye!"
 end 
